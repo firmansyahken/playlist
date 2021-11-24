6952730da35949759d16f837da6322ec0e8e7beb
W_Firmansyah <66572675+firmansyahken@users.noreply.github.com>
2021-11-24 20:25:49 +0700

    Add files via upload
    Deploy

---
+++ main.js
@@ -0,0 +1,139 @@
+const header = document.querySelector(".header");
+const play_now = document.getElementById("play_now");
+const list_music = document.querySelector(".list_music");
+var button_plays;
+var music = new Audio;
+var playing = false;
+var playTrack;
+var idTrack;
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("active", this.scrollY > 0)
+})
+
+play_now.addEventListener("click", playNow)
+
+function playNow() {
+    const sfx = new Audio("music/sfx.mp3")
+    sfx.play()
+    let target = "."+this.dataset.target
+    const offsetTop = document.querySelector(target).offsetTop
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+
+}
+
+async function getData() {
+    let fetchData = await fetch("db.json")
+    let responses = await fetchData.json()
+    let playlists = responses.playlist
+    renderData(playlists)
+}
+
+getData()
+
+function renderData(playlists) {
+    let template = ''
+    let i = 1
+    playlists.map(playlist => {
+        template += `
+        <li>
+            <div class="detail">
+                <p>${i++}. ${playlist.title}</p>
+            </div>
+            <div class="play">
+                <i class="fa fa-play" id="btn_play" data-id="${playlist.src}" data-list="${i - 1}""></i>
+            </div>
+        </li>`
+    })
+
+    list_music.innerHTML = template
+    
+    button_plays = document.querySelectorAll("#btn_play");
+
+    button_plays.forEach(btn_play => {
+        btn_play.addEventListener("click", PlayMusic)
+    })
+
+
+    music.addEventListener("ended", function() {
+        let idList = parseInt(idTrack) + 1
+        if(idList > playlists.length) {
+            return
+        }
+
+        let src_music = playlists[idTrack].src;
+        let title = playlists[idTrack].title;
+        let listNow = document.querySelector(`[data-list="${idList}"]`)
+        if(playing) {
+            if(playTrack != src_music) {
+                playing = true
+                music.src = "music/"+src_music
+                idTrack = src_music
+                listNow.setAttribute("class", "fa fa-pause")
+                music.play()
+            } else {
+                music.pause()
+                playing = false
+                this.setAttribute("class", "fa fa-play")
+            }
+        } else {
+            playing = true
+            this.setAttribute("class", "fa fa-pause")
+    
+            if(playTrack != src_music) {
+                playing = true
+                music.src = "music/"+src_music
+                idTrack = src_music
+            }
+            music.play()
+        }
+    
+        playTrack = src_music
+    })
+    
+    
+}
+
+function PlayMusic() {
+
+    button_plays.forEach(btn_play => {
+        btn_play.setAttribute("class", "fa fa-play")
+    })
+
+    if(playing) {
+        if(playTrack != this.dataset.id) {
+            playing = true
+            music.src = "music/"+this.dataset.id
+            idTrack = this.dataset.list
+            this.setAttribute("class", "fa fa-pause")
+            music.play()
+        } else {
+            music.pause()
+            playing = false
+            this.setAttribute("class", "fa fa-play")
+        }
+    } else {
+        playing = true
+        this.setAttribute("class", "fa fa-pause")
+
+        if(playTrack != this.dataset.id) {
+            playing = true
+            music.src = "music/"+this.dataset.id
+            idTrack = this.dataset.list
+        }
+        music.play()
+    }
+
+    playTrack = this.dataset.id
+    
+}
+
+music.addEventListener("ended", function() {
+    button_plays.forEach(btn_play => {
+        btn_play.setAttribute("class", "fa fa-play")
+    })
+})
+
